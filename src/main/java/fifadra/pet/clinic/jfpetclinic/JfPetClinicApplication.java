1b359db909bf5b67b50ae0ecd8ebc51c6f53c7fa
jaydeepfifadra <jaydeepfifadra@gmail.com>
2018-11-16 18:16:56 +0530

    Initial code add

---
+++ src/main/java/fifadra/pet/clinic/jfpetclinic/JfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package fifadra.pet.clinic.jfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfPetClinicApplication.class, args);
+    }
+}
