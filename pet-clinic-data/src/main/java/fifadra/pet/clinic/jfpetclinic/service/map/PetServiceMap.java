98b7f604bc71ed000786f0d2759c39eeee421d14
jaydeepfifadra <jaydeepfifadra@live.com>
2019-01-01 19:28:46 +0530

    Implement map based service. closes #6

---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,32 @@
+package fifadra.pet.clinic.jfpetclinic.service.map;
+
+import fifadra.pet.clinic.jfpetclinic.model.Pet;
+import fifadra.pet.clinic.jfpetclinic.service.CrudService;
+
+import java.util.Set;
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package fifadra.pet.clinic.jfpetclinic.service.map;
+
+import fifadra.pet.clinic.jfpetclinic.model.Vet;
+import fifadra.pet.clinic.jfpetclinic.service.CrudService;
+import fifadra.pet.clinic.jfpetclinic.service.map.AbstractMapService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
