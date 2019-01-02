a25769b2cbd06e3915aa81b614d04db25663f800
jaydeepfifadra <jaydeepfifadra@live.com>
2019-01-02 21:46:03 +0530

    Adding Owner index page and controller. closes #13

---
+++ pet-clinic-web/src/main/java/fifadra/pet/clinic/jfpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package fifadra.pet.clinic.jfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
+    public String ListOfOwners(){
+        return "owners/index";
+    }
+}
