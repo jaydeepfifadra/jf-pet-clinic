bec94f936a4937e6f2f796567212fe5d79dbcc14
jaydeepfifadra <jaydeepfifadra@live.com>
2019-01-06 20:38:32 +0530

    Added Owner's list and updated controller. closes #16

--- pet-clinic-web/src/main/java/fifadra/pet/clinic/jfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/fifadra/pet/clinic/jfpetclinic/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package fifadra.pet.clinic.jfpetclinic.controllers;
 
+import fifadra.pet.clinic.jfpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners","/owners/index","/owners/index.html"})
-    public String ListOfOwners(){
+    public String ListOfOwners(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
     }
 }
