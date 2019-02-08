e3b6cfca29f172455b204e7bc4bae6bd6cf891d6
jaydeepfifadra <jaydeepfifadra@live.com>
2019-02-08 21:06:27 +0530

    Added PatType Service. closes #29

---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package fifadra.pet.clinic.jfpetclinic.service;
+
+import fifadra.pet.clinic.jfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package fifadra.pet.clinic.jfpetclinic.service.map;
+
+import fifadra.pet.clinic.jfpetclinic.model.PetType;
+import fifadra.pet.clinic.jfpetclinic.service.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
