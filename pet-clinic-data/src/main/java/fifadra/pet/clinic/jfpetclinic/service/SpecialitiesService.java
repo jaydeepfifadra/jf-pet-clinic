40ba9915407ad3255189e4e26471acb1519ff9ba
jaydeepfifadra <jaydeepfifadra@live.com>
2019-02-08 22:11:06 +0530

    Added Speciality service. closes #31

---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package fifadra.pet.clinic.jfpetclinic.service;
+
+import fifadra.pet.clinic.jfpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
