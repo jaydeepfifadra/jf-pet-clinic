6d7982231b9f968d1dd6c25901033a6b16b243f6
jaydeepfifadra <jaydeepfifadra@live.com>
2019-02-08 20:49:42 +0530

    Added Speciality, updated Vet with speciality relationship. closes #27

---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package fifadra.pet.clinic.jfpetclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
