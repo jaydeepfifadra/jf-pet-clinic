ab79f80d1629be429f3700fb9348af94ad886bc5
jaydeepfifadra <jaydeepfifadra@live.com>
2019-03-01 09:18:54 +0530

    Added spring data JPA service for  vets. closes #41

---
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/service/jpaservice/VetJpaService.java
@@ -0,0 +1,48 @@
+package fifadra.pet.clinic.jfpetclinic.service.jpaservice;
+
+import fifadra.pet.clinic.jfpetclinic.model.Vet;
+import fifadra.pet.clinic.jfpetclinic.repositories.VetRepository;
+import fifadra.pet.clinic.jfpetclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetJpaService implements VetService {
+
+    VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
