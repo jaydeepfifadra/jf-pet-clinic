022fe27fd3e2db8a35f133a7e8ea78dc7e92f34a
jaydeepfifadra <jaydeepfifadra@live.com>
2019-03-03 14:52:54 +0530

    Added project Lombok. closes #51

--- pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/fifadra/pet/clinic/jfpetclinic/model/Vet.java
@@ -1,8 +1,15 @@
 package fifadra.pet.clinic.jfpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet extends Person{
@@ -10,12 +17,4 @@ public class Vet extends Person{
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="vet_specialities",joinColumns =  @JoinColumn(name="vet_id"),inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
