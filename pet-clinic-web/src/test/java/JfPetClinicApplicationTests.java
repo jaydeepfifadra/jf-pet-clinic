5fa85ad4ae7f48f9e421f4992b83868b9e2b0bbf
jaydeepfifadra <jaydeepfifadra@live.com>
2019-04-07 22:23:29 +0530

    JUNIT 5

--- pet-clinic-web/src/test/java/JfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/JfPetClinicApplicationTests.java
@@ -1,9 +1,6 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SprintExtenstion.class)
 @SpringBootTest
 public class JfPetClinicApplicationTests {
 
